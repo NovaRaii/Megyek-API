6957fc095242466d52e8d35d29c040162d5580e9
NovaRaii <103244747+NovaRaii@users.noreply.github.com>
2024-09-23 09:30:07 +0200

    find, delete

---
+++ src/Html/Request.php
@@ -0,0 +1,111 @@
+<?php
+ 
+namespace App\Html;
+ 
+use App\Repositories\CountyRepository;
+ 
+class Request {
+ 
+    static function handle(): void {
+        switch ($_SERVER["REQUEST_METHOD"]){
+            case "POST":
+                self::postRequest();
+                break;
+            case "GET":
+                self::getRequest();
+                break;
+            case "PUT":
+                self::putRequest();
+                break;
+            case "DELETE":
+                self::deleteRequest();
+                break;
+            default:
+                echo 'Unknown request type';
+                break;
+        }
+    }
+ 
+    private static function getRequest(): void
+    {
+        $resourceName = self::getResourceName();
+        switch ($resourceName){
+            case 'counties':
+                $db = new CountyRepository();
+                $resourceId = self::getResourceId();
+                $code = 200;
+                if($resourceId){
+                    $entity = $db->find($resourceId);
+                    Response::response($entity, $code);
+                    break;
+                }
+ 
+                $enteties = $db->getAll();
+                if(empty($enteties)){
+                    $code = 404;
+                }
+                Response::response($enteties, $code);
+                break;
+ 
+            default:
+                Response::response([], 404,  $_SERVER['REQUEST_URI'] . " not found");
+        }
+    }
+ 
+
+    private static function getArrUri(string $requestUri): ?array
+        {
+            return explode("/", $requestUri) ?? null;
+        }
+        
+        private static function getResourceName(): string
+        {
+            $arrUri = self::getArrUri($_SERVER['REQUEST_URI']);
+            $result = $arrUri[count($arrUri) - 1];
+            if(is_numeric($result))
+            {
+                $result = $arrUri[count($arrUri) - 2];
+            }
+
+            return $result;
+        }
+
+        private static function getResourceId(): int
+        {
+            $arrUri = self::getArrUri($_SERVER['REQUEST_URI']);
+            $result = 0;
+            if(is_numeric($arrUri[count($arrUri) - 1]))
+            {
+                $result = $arrUri[count($arrUri) - 1];
+            }
+            return $result;
+        }
+
+        private static function deleteRequest(){
+            $resourceName = self::getResourceName();
+            $resourceId = self::getResourceId();
+        switch ($resourceName){
+            case 'counties':
+                $db = new CountyRepository();
+                if($resourceId){
+                    $entity = $db->find($resourceId);
+                    if ($entity) {
+                        $db->delete($resourceId);
+                        Response::response([],204);
+
+                    }
+                    else {
+                        Response::response([], 404, $_SERVER['REQUEST_URI'] . " not found");
+                    }
+                    break;
+                default:
+                Response::response([], 404, $_SERVER['REQUEST_URI'] . " not found");
+                }
+        }
+    }
+ 
+        }
+}
+ 
+ 
+?>
---
+++ src/Repositories/BaseRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Database\DB;
+
+class BaseRepository extends DB
+{
+    protected string $tableName;
+
+    public function getAll(): array
+    {
+        $query = $this->select() . "ORDER BY name";
+
+        return $this->mysqli->query($query)->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function select(): string
+    {
+        return "SELECT * FROM `{$this->tableName}` ";
+    }
+
+    public function find($id): array 
+    {
+        $query = "SELECT * FROM `{$this->tableName}` WHERE id = {$id}";
+        return $this->mysqli->query($query)->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function delete($id) {
+        $query = "SELECT * FROM `{$this->tableName}` WHERE id = {$id}";
+        return $query;
+    }
+
+}
